1f3c9151c8298fccd48f76b4939250559766dc2a
Vladimir D <bearsbon@gmail.com>
2023-12-04 00:53:50 +0500

    readme + types

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,30 @@ import axios from "axios";
 
 export default function App() {
   const [number, setNumber] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [error, setError] = useState(null);
 
-  const handleChange = (e) => {
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  const handleChangeEmail = (e) => {
+    if (!isValidEmail(e.target.value)) {
+      setError("Email is invalid");
+    } else {
+      setError(null);
+    }
+
+    setEmail(e.target.value);
+  };
+
+  const handleChangeNumber = (e) => {
+    if (e.target.value.length < 8) {
+      setError("Number should contain 6 digits");
+    } else {
+      setError(null);
+    }
+
     const mask = IMask(document.getElementById("masked"), {
       mask: "00-00-00",
     });
@@ -14,20 +36,26 @@ export default function App() {
 
   const CancelToken = axios.CancelToken;
   let source = CancelToken.source();
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (error || number.length < 8) {
+      setError("Please fill all inputs");
+      return;
+    }
+
     source && source.cancel("Last request was canceled");
     source = axios.CancelToken.source();
 
     axios
       .post(
         "http://localhost:3000/",
-        { number: number },
+        { email: email, number: number },
         {
           cancelToken: source.token,
         }
       )
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -36,13 +64,20 @@ export default function App() {
 
   return (
     <>
-      <div style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
-        <input type="email" />
-        <input id="masked" onChange={(e) => handleChange(e)} type="text" />
-      </div>
-      <div>
-        <button onClick={(event) => handleSubmit(event)}>Submit</button>
-      </div>
+      <form>
+        <div style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
+          <input type="email" onChange={(e) => handleChangeEmail(e)} />
+          <input
+            id="masked"
+            onChange={(e) => handleChangeNumber(e)}
+            type="text"
+          />
+        </div>
+        <div>
+          <button onClick={(event) => handleSubmit(event)}>Submit</button>
+        </div>
+      </form>
+      {error && <h2 style={{ color: "red" }}>{error}</h2>}
     </>
   );
 }
